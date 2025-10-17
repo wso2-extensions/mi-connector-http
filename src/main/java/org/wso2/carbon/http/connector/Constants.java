c6e1e4f5e848cfe58164054b50a947750508db2b
chathurangaj <chathuranga.jayanath.1999@gmail.com>
2025-10-17 11:24:29 +0530

    Fix base path relative path concatenation when relative path is empty

--- src/main/java/org/wso2/carbon/http/connector/Constants.java
+++ src/main/java/org/wso2/carbon/http/connector/Constants.java
@@ -44,4 +44,5 @@ public class Constants {
     public static final String SOAP12_CONTENT_TYPE = "application/soap+xml";
     public static final String EMPTY_LIST = "[]";
     public static final String FORWARD_SLASH = "/";
+    public static final String QUESTION_MARK = "?";
 }
